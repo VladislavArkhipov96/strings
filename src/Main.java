34cc8629ca23c6857036ce6230fb55475c524d34
VladislavArkhipov96 <y906ee@mail.ru>
2024-08-20 23:40:02 +0300

    edited

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,30 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
 
+
+
+        //Task 1
+        System.out.println("Task 1");
+
+        String firstName = "ivan";
+        String middleName = "Ivanovich";
+        String lastName = "ivanov";
+
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        //Task 2
+        System.out.println("Task 2");
+
+        String fullNameInUpperCase = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullNameInUpperCase);
+
+        //Task 3
+        System.out.println("Task 3");
+
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
+
     }
 }
